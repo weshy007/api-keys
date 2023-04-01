37bd932e06e09738a3154fcda5dffa7790543545
Weshy Joe <josephwaweru96@gmail.com>
2023-04-01 12:46:04 +0300

    serializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
+
 
 # Create your views here.
+print(get_random_string(32, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
