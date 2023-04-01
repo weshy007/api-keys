c31a932a4e38d6e90214273c2edf976caebf9c54
Weshy Joe <josephwaweru96@gmail.com>
2023-04-01 13:37:44 +0300

    final commit

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from dj_rest_auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
-
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('api-keys/', views.APIKeysView.as_view(), name='api-keys'),
+    path('validate-api-keys/', views.ValidateAPIKeys.as_view(), name='validate-api-keys')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,61 @@
+import random
+import string
+
 from django.shortcuts import render
 from django.utils.crypto import get_random_string
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import CustomUser
 
 
 # Create your views here.
-print(get_random_string(32, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
+def generate_random_string(length: int) -> str:
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+
+class APIKeysView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = CustomUser.objects.get(id=request.user.id)
+
+        if user:
+            return Response(data={"api_key": user.api_key}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user = CustomUser.objects.get(id=request.user.id)
+        if user:
+            api_key = get_random_string(
+                32, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            )
+            api_secret = get_random_string(
+                64, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            )
+
+            user.api_key = api_key
+            user.api_secret = make_password(api_secret)
+            user.save()
+
+            return Response(data={"api_key": api_key, "api_secret": api_secret}, status=status.HTTP_200_OK)
+
+
+class ValidateAPIKeys(APIView):
+    def post(self, request):
+        api_key = request.headers.get("api-key")
+        api_secret = request.headers.get("api-secret")
+
+        try:
+            user = CustomUser.objects.get(api_key=api_key)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if user:
+            if user.api_key == api_key and user.has_valid_api_secret(api_secret):
+                return Response(data={"valid": True}, status=status.HTTP_200_OK)
+            return Response(data={"valid": False}, status=status.HTTP_200_OK)
